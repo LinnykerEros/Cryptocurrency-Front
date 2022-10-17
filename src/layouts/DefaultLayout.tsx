80996845bca95bea4e55338a6c9d7cd929223cac
Eros Linnyker <linnykereros@gmail.com>
2022-10-17 19:56:44 -0300

    added: layout

---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@mui/material";
+import Header from "../components/Header";
+function DefaultLayout() {
+  return (
+    <Box sx={{}}>
+      <Header />
+    </Box>
+  );
+}
+
+export default DefaultLayout;
