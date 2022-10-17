4bc286573da6fdf896b948f1e97a490f17a71f9d
Eros Linnyker <linnykereros@gmail.com>
2022-10-17 19:56:08 -0300

    adjusments

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import { ThemeProvider as UiThemeProvider } from "@mui/material/styles";
+import { theme } from "./styles/theme";
+import { BrowserRouter } from "react-router-dom";
+import Router from "./Router";
+import { CryptocurrencyContentProvider } from "./context/ContentContext";
+import Header from "./components/Header";
+import "./styles/Global.module.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UiThemeProvider theme={theme}>
+      <CryptocurrencyContentProvider>
+        <BrowserRouter>
+          <Header />
+
+          <Router />
+        </BrowserRouter>
+      </CryptocurrencyContentProvider>
+    </UiThemeProvider>
   );
 }
 
